d804481d143e2140d22ac2144b249ded021351f9
v08nike <teddan817@gmail.com>
2023-02-08 04:27:40 -0800

    Upd: Menu

--- src/layouts/main/MenuConfig.js
+++ src/layouts/main/MenuConfig.js
@@ -1,14 +1,14 @@
 // components
-import SvgIconStyle from '../../components/SvgIconStyle';
+import Iconify from '../../components/Iconify';
 
 // ----------------------------------------------------------------------
 
-const getIcon = (name) => <SvgIconStyle src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />;
+const getIcon = (name) => <Iconify icon={name} sx={{ width: 1, height: 1 }} />;
 
 const ICONS = {
-  user: getIcon('ic_user'),
-  dashboard: getIcon('ic_dashboard'),
-  download: getIcon('ic_download'),
+  user: getIcon('mdi:user-outline'),
+  dashboard: getIcon('grommet-icons:projects'),
+  download: getIcon('ic:outline-download'),
 };
 
 const navConfig = [
@@ -17,7 +17,7 @@ const navConfig = [
   {
     subheader: 'general',
     items: [
-      { title: 'My Jobs', path: '/jobs', icon: ICONS.dashboard },
+      { title: 'Project', path: '/jobs', icon: ICONS.dashboard },
       { title: 'My Account', path: '/account', icon: ICONS.user },
       { title: 'Resources', path: '/resources', icon: ICONS.download },
     ],
